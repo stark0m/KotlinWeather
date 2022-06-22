56f29578b58114b76cd2d86f51d464cd67bf9be5
ilya <011283im@gmail.com>
2022-06-22 09:59:59 +0300

    5. Потренироваться в создании классов и функций, описанных в уроке, и убедиться, что всё работает. Например, создать тестовое приложение: a. Сформировать data class с двумя свойствами и вывести их на экран приложения. b. Создать Object. В Object вызвать copy и вывести значения скопированного класса на экран.

---
+++ app/src/main/java/com/example/kotlinweather/AllData.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinweather
+
+
+data class Person (val name:String,var weight:Int)
--- app/src/main/java/com/example/kotlinweather/MainActivity.kt
+++ app/src/main/java/com/example/kotlinweather/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,20 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         findViewById<MaterialButton>(R.id.mainactivity_click_me_button).setOnClickListener(){
 
+            doAction()
         }
+    }
+
+
+    fun localToast(text:String){
+        Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT).show()
+    }
+    private fun doAction() {
+        val andrey = Person("Andrey",45)
+        localToast("${andrey.name} весит ${andrey.weight}")
+        val obj = object {
+            val clone= andrey.copy(weight = 49)
+              }
+        localToast("появился клон по имени ${obj.clone.name} и весом ${obj.clone.weight}")
     }
 }
