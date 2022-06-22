c52dd9a7f78089b6fd9b7a2b89019108f3d2614f
ilya <011283im@gmail.com>
2022-06-22 09:48:17 +0300

    4. Добавить кнопку в разметку и повесить на неё clickListener в Activity.

--- app/src/main/java/com/example/kotlinweather/MainActivity.kt
+++ app/src/main/java/com/example/kotlinweather/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.kotlinweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        findViewById<MaterialButton>(R.id.mainactivity_click_me_button).setOnClickListener(){
+
+        }
     }
 }
