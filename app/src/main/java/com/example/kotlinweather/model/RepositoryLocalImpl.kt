680382b635e6cceda5d789ebebe1f8780afa35e9
ilya <011283im@gmail.com>
2022-06-25 16:37:31 +0300

    раализация колбеков от репозитория и передача соответствующий данных в лайвдейту рандомно передает либо ошибку загрузки данных либо успешно передает информацию дефолтного значения на фрагмент помещен прогрессбар

--- app/src/main/java/com/example/kotlinweather/model/RepositoryLocalImpl.kt
+++ app/src/main/java/com/example/kotlinweather/model/RepositoryLocalImpl.kt
@@ -5,18 +5,17 @@ import com.example.kotlinweather.domain.getDefaultCity
 import java.lang.Thread.sleep
 import kotlin.concurrent.thread
 
-class RepositoryLocalImpl:Repository {
+class RepositoryLocalImpl : Repository {
     override fun getWeatherLIst(weatherList: WeatherCallBack<List<Weather>>) {
         weatherList.onDataReceived(mutableListOf())
     }
 
     override fun getWeather(lat: Double, lon: Double, weather: WeatherCallBack<Weather>) {
 
-       thread {
-           sleep(2000L)
-           weather.onDataReceived(Weather(getDefaultCity()))
-       }.start()
+        thread {
+            sleep(2000L)
+            weather.onDataReceived(Weather(getDefaultCity()))
+        }.start()
 
-    }
     }
 }
--- app/src/main/java/com/example/kotlinweather/view/weathershow/WeatherShowViewModel.kt
+++ app/src/main/java/com/example/kotlinweather/view/weathershow/WeatherShowViewModel.kt
@@ -2,25 +2,44 @@ package com.example.kotlinweather.view.weathershow
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.kotlinweather.domain.Weather
 import com.example.kotlinweather.model.Repository
 import com.example.kotlinweather.model.RepositoryLocalImpl
 import com.example.kotlinweather.model.RepositoryNetworkImpl
+import com.example.kotlinweather.model.WeatherCallBack
 import com.example.kotlinweather.viewmodel.AppState
 import com.example.kotlinweather.viewmodel.ViewModelInterface
 
 class WeatherShowViewModel(
     private val vmLiveData: MutableLiveData<Any> = MutableLiveData()
-) : ViewModel(),ViewModelInterface{
+) : ViewModel(), ViewModelInterface {
     var repository: Repository? = null
 
     private fun isConnected() = false
 
     override fun getData() {
         chooseRepository()
+
         vmLiveData.value = AppState.Loading
+
+        repository!!.getWeather(55.755826, 37.617299900000035, object : WeatherCallBack<Weather> {
+            override fun onDataReceived(result: Weather) {
+                if (isWeatherReceived(result)) {
+                    vmLiveData.value = AppState.Success(result)
+                } else {
+                    vmLiveData.value =
+                        AppState.Error(throw IllegalStateException("Данные не были получены из препозитория"))
+                }
+            }
+
+        })
+
+
 
 
     }
+
+    private fun isWeatherReceived(any: Any): Boolean = (0..5).random() == 1
 
     private fun chooseRepository() {
         repository = if (isConnected()) {
