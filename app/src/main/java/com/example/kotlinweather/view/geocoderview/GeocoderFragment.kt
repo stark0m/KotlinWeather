0fe69f7b9672240a947d79070f78bfab992876bd
ilya <011283im@gmail.com>
2022-07-19 10:09:09 +0300

    Урок 10 , добавили фрагмент для геокодера

---
+++ app/src/main/java/com/example/kotlinweather/view/geocoderview/GeocoderFragment.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinweather.view.geocoderview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kotlinweather.R
+
+
+class GeocoderFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_geocoder, container, false)
+    }
+
+    companion object {
+
+        fun newInstance() =
+            GeocoderFragment()
+    }
+}
